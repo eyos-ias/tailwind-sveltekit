cbe957c82782ef810a77b7956346b6151a5a9081
eyos <eyosiastamirat20@gmail.com>
2023-02-11 14:32:39 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex items-center justify-center text-blue-700 h-screen">
+    <h1 class="text-6xl font-bold">Hello, World!</h1>
+  </div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.blue.100);
+  }
+</style>
